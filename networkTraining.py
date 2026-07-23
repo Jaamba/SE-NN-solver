31af16227d31dfe291c72c97d55f0b36c2aa2d43
Vincenzo Illiano <illianovincenzo05@gmail.com>
2026-07-23 14:34:49 +0200

    Add training from dataset

--- networkTraining.py
+++ networkTraining.py
@@ -2,61 +2,71 @@ import torch
 from torch import optim
 import network
 import helper
+import datasetGen
 import torch.nn.functional as F
 
 # Uses the GPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
+print("Using device: " + str(device))
 
 # Build the network
 model = network.SESolver().to(device)
 
 # Loss function definition for the network
-def criterion(E, phi, Hphi, V):
+def criterion(E, phi, Hphi, V, Ebatch):
     # Minimizing this will make sure that phi is a solution. Only considers inner values
     mse = torch.mean(
-        (Hphi[:,1:-1] - E*phi[:,1:-1])**2
+        (Hphi[:,1:-1] - Ebatch*phi[:,1:-1])**2
     )
 
     # Rewards the solver if it selected energies close to the ground state
-    Eground, _ = helper.solve_schrodinger(V, network.dt, 0)
-    groundLoss = torch.mean( (E - Eground)**2 )
+    groundLoss = torch.mean( (E - Ebatch)**2 )
 
     return mse + groundLoss
 
 # Model optimizer to train the model
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
-# Number of batches to generate
-pols = 100
-# batch size
-BATCH_SIZE = 512
 # Number of epochs
 epoch = 1
 
+# Loads the dataset
+print("Loading dataset...")
+dataset = torch.load( "trainingset.pt", map_location="cpu")
+print("Dataset loaded corretctly")
+
+# Loads the Energy set
+print("Loading energy set...")
+Eset = torch.load( "ground_energy_set.pt", map_location="cpu")
+
+BATCH_SIZE = datasetGen.BATCH_SIZE
+NUM_BATCHES = datasetGen.NUM_BATCHES
+
 # Makes sure to be in training mode
 model.train()
-for i in range(pols):
+for i, (batch, Ebatch) in enumerate(zip(dataset, Eset)):
+
+    # Moves the batch to the used device
+    batch = batch.to(device, non_blocking=True)
+    Ebatch = Ebatch.to(device, non_blocking=True)
 
     # Prints current training progress
-    if i % (pols/100) == 0:
-        print("Current progress:", i/pols*100, "%") 
-
-    # Input values for the network
-    input = helper.random_function(BATCH_SIZE, network.N, device=device, sigma=32)
+    if i % (NUM_BATCHES/100) == 0:
+        print(f"Current progress: {i/NUM_BATCHES*100}%", end="\r") 
 
     for j in range(epoch):
         optimizer.zero_grad()
 
         # Forward press
-        E, phi, Hphi = model(input)
+        E, phi, Hphi = model(batch)
 
         # Calculates the loss
-        loss = criterion(E, phi, Hphi, input)
+        loss = criterion(E, phi, Hphi, batch, Ebatch)
 
         # Bacward press
         loss.backward()
         optimizer.step()
+print()
 
 # Saves the trained model
 torch.save(model.state_dict(), 'checkpoint.pth')
